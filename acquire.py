4790e09a9804ba7fea092829055dbb05859603a2
Corey Solitaire <corey.solitaire@gmail.com>
2020-11-18 12:16:50 -0600

    Setup

---
+++ acquire.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import numpy as np
+import requests
+from requests import get
+import re
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+######################################## Web Scraping Functions #########################################
+
