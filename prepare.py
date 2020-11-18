87772773572d9afad51d72060d9bbeb2427c3838
Corey Solitaire <corey.solitaire@gmail.com>
2020-11-18 16:43:46 -0600

    Function Work

--- prepare.py
+++ prepare.py
@@ -17,72 +17,78 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 ###################################### Clean Data Function ###############################
 
+def basic_clean(text):
+    '''
+    Converts text in to ascii to remove special characters, then converts back in to utf-8
+    '''
+    text = (unicodedata.normalize('NFKD', text.lower())
+            .encode('ascii', 'ignore') # ascii to reduce noise
+            .decode('utf-8', 'ignore') # decode using utf-8
+           )
+    return re.sub(r"[^a-z0-9\s]", '', text)
+
+def tokenize(string):
+    '''
+    This function takes in a string and
+    returns a tokenized string.
+    '''
+    # Create tokenizer.
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    
+    # Use tokenizer
+    string = tokenizer.tokenize(string, return_str=True)
+    
+    return string
+
+def lemmatize(string):
+    '''
+    This function takes in string for and
+    returns a string with words lemmatized.
+    '''
+    # Create the lemmatizer.
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # Use the lemmatizer on each word in the list of words we created by using split.
+    lemmas = [wnl.lemmatize(word) for word in string.split()]
+    
+    # Join our list of words into a string again and assign to a variable.
+    string = ' '.join(lemmas)
+    
+    return string
+
+
+
+def remove_stopwords(string, extra_words=[], exclude_words=[]):
+    '''
+    This function takes in a string, optional extra_words and exclude_words parameters
+    with default empty lists and returns a string.
+    '''
+    # Create stopword_list.
+    stopword_list = stopwords.words('english')
+    
+    # Remove 'exclude_words' from stopword_list to keep these in my text.
+    stopword_list = set(stopword_list) - set(exclude_words)
+    # Add in 'extra_words' to stopword_list.
+    stopword_list = stopword_list.union(set(extra_words))
+    
+    # Split words in string.
+    words = string.split()
+    
+    # Create a list of words from my string with stopwords removed and assign to variable.
+    filtered_words = [word for word in words if word not in stopword_list]
+    
+    # Join words in the list back into strings and assign to a variable.
+    string_without_stopwords = ' '.join(filtered_words)
+    
+    return string_without_stopwords
+
+    
 def clean_data(df):
     '''
     This function takes in a dataframe of text, cleans, tokenizes, lemmatizes, and removes stopwords
     from that text, appending each step in the process to the dataframe.  It also appends a list of 
     words from each article as well as the total lenght.  
-    '''
-    def basic_clean(text):
-        text = (unicodedata.normalize('NFKD', text.lower())
-                .encode('ascii', 'ignore') # ascii to reduce noise
-                .decode('utf-8', 'ignore') # decode using utf-8
-               )
-        return re.sub(r"[^a-z0-9\s]", '', text)
-
-    def tokenize(string):
-        '''
-        This function takes in a string and
-        returns a tokenized string.
-        '''
-        # Create tokenizer.
-        tokenizer = nltk.tokenize.ToktokTokenizer()
-
-        # Use tokenizer
-        string = tokenizer.tokenize(string, return_str=True)
-
-        return string
-
-    def lemmatize(string):
-        '''
-        This function takes in string for and
-        returns a string with words lemmatized.
-        '''
-        # Create the lemmatizer.
-        wnl = nltk.stem.WordNetLemmatizer()
-
-        # Use the lemmatizer on each word in the list of words we created by using split.
-        lemmas = [wnl.lemmatize(word) for word in string.split()]
-
-        # Join our list of words into a string again and assign to a variable.
-        string = ' '.join(lemmas)
-
-        return string
-
-    def remove_stopwords(string, extra_words=[], exclude_words=[]):
-        '''
-        This function takes in a string, optional extra_words and exclude_words parameters
-        with default empty lists and returns a string.
-        '''
-        # Create stopword_list.
-        stopword_list = stopwords.words('english')
-
-        # Remove 'exclude_words' from stopword_list to keep these in my text.
-        stopword_list = set(stopword_list) - set(exclude_words)
-        # Add in 'extra_words' to stopword_list.
-        stopword_list = stopword_list.union(set(extra_words))
-
-        # Split words in string.
-        words = string.split()
-
-        # Create a list of words from my string with stopwords removed and assign to variable.
-        filtered_words = [word for word in words if word not in stopword_list]
-
-        # Join words in the list back into strings and assign to a variable.
-        string_without_stopwords = ' '.join(filtered_words)
-
-        return string_without_stopwords
-    
+    '''    
     # Formatt title, makes it easier to read
     df['title'] = df.title.apply(basic_clean)
     df['title'] = df.title.apply(tokenize)
