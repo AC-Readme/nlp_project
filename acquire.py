ba4d2d05db360716f806a5f3569cb387facd109e
Angel Gomez <gomez.i.angel3@gmail.com>
2020-11-21 00:35:09 -0600

    edits

--- acquire.py
+++ acquire.py
@@ -213,3 +213,50 @@ def get_readme_articles(urls, cached=False):
     
     return df
 
+##### Function to scrape the readme without creating a json file #####
+
+def get_readme_articles_func(urls, cached=False):
+    '''
+    This function takes in a list of GitHub Repo urls and a parameter
+    with default cached == False which scrapes the title, text, and language for each url, 
+    creates a list of dictionary features, converts list to df, and returns df.
+    If cached == True, the function returns a df from a json file.
+    '''
+    if cached == True:
+        df = pd.read_json('project_readme.json')
+        
+    # cached == False completes a fresh scrape for df     
+    else:
+
+        # Create an empty list to hold dictionaries
+        text = []
+
+        # Loop through each url in our list of urls
+        for url in urls:
+
+            # Make request and soup object using helper
+            soup = make_soup(url)
+
+            # Save the title of each repo in variable title
+            title = soup.select('h1', class_="Label Label--outline v-align-middle")[0].text
+
+            # Save the text in each repo to variable text
+            content = soup.select('article', class_="markdown-body entry-content container-lg")[0].text
+            
+            # Save the language of each repo in variable language
+            language = soup.select('li.d-inline:nth-child(1) > a:nth-child(1)')[0].text
+
+            # Create a dictionary holding the title and content for each blog
+            repo = {'title': title, 'content': content, 'language': language}
+
+            # Add each dictionary to the articles list of dictionaries
+            text.append(repo)
+            
+        # convert our list of dictionaries to a df
+        df = pd.DataFrame(text)
+
+        # Write df to a json file for faster access
+        #df.to_json('project_readme.json')
+    
+    return df
+
